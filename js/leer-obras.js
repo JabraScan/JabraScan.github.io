5b57ef949af40d597eb5458dba32d9f0a6830fae
JabraScan <Jabracadabrap@gmail.com>
2025-08-23 22:41:38 +0200

    Add files via upload

---
+++ js/leer-obras.js
@@ -0,0 +1,50 @@
+// leer-obras.js
+fetch('obras.xml')
+  .then(response => response.text())
+  .then(str => new window.DOMParser().parseFromString(str, "text/xml"))
+  .then(data => {
+    const obras = data.querySelectorAll("obra");
+    const carouselContainer = document.getElementById("carousel-track");
+    const booklistContainer = document.getElementById("book-list");
+
+    obras.forEach(obra => {
+      const clave = obra.querySelector("clave").textContent;
+      const nombreobra = obra.querySelector("nombreobra").textContent;
+      const autor = obra.querySelector("autor").textContent;
+      const sinopsis = obra.querySelector("sinopsis").textContent;
+      const imagen = obra.querySelector("imagen").textContent;
+      const valoracion = obra.querySelector("valoracion").textContent;
+      const tipoobra = obra.querySelector("tipoobra").textContent;
+      const Categoria = obra.querySelector("categoria").textContent;
+
+      const itemCarousel = document.createElement("div");
+	      itemCarousel.className = "carousel-itemCarousel";
+	      itemCarousel.innerHTML = `
+	        <img src="${imagen}" alt="${nombreobra}">
+	        <div class="carousel-info-overlay">
+	          <div class="carousel-info-title">${nombreobra}</div>
+	          <div class="carousel-info-row">
+	            <span class="carousel-info-label">Valoración:</span> ${valoracion}
+	            <span class="carousel-info-label">Tipo:</span> ${tipoobra}
+	            <span class="carousel-info-label">Categoría:</span> ${Categoria}
+	          </div>
+	        </div>
+	      `;
+	carouselContainer.appendChild(itemCarousel);
+      const itemBook = document.createElement("article");
+	      itemBook.className = "book-card";
+	      itemBook.innerHTML = `
+		        <a href="books/${clave}/index.html">
+		          <img src="${imagen}" alt="${nombreobra}" loading="lazy"/>
+		        </a>
+		          <div class="book-info">
+		            <h2>${nombreobra}</h2>
+		            <p class="book-author"><div class="book-author-title">Autor:</div><div class="book-author-name">${autor}</div></p>
+		            <p class="book-sinopsis">Sinopsis: ${sinopsis}</p>
+		          </div>
+	      `;
+	booklistContainer.appendChild(itemBook);
+
+    });
+  })
+  .catch(err => console.error("Error al cargar el XML:", err));
