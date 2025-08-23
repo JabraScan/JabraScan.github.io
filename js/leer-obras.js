a67789b83854ef6a1dae062ee24c6a4ce48a76e4
JabraScan <Jabracadabrap@gmail.com>
2025-08-24 01:25:21 +0200

    Update leer-obras.js

--- js/leer-obras.js
+++ js/leer-obras.js
@@ -41,7 +41,7 @@ fetch('obras.xml')
 		          <img src="${imagen}" alt="${nombreobra}" loading="lazy"/>
 		        </a>
 		          <div class="book-info">
-		            <h2>${nombreobra}</h2>
+		            <h3>${nombreobra}</h3>
 		            <p class="book-author"><div class="book-author-title">Autor:</div><div class="book-author-name">${autor}</div></p>
 		            <p class="book-sinopsis tooltip">
 			  			<b>Sinopsis:</b>
