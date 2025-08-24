78f7ed236537f286eb4d72b843f3dd8882665bcc
JabraScan <Jabracadabrap@gmail.com>
2025-08-24 21:26:32 +0200

    Update general.js

--- js/general.js
+++ js/general.js
@@ -2,6 +2,17 @@ document.addEventListener("DOMContentLoaded", function () {
   // Evento para abrir el disclaimer en el main
     document.getElementById('disclaimer-link').addEventListener('click', function (e) {
       e.preventDefault();
-      document.getElementById('main').innerHTML = "disclaimer.html";
+      // Usar fetch para cargar el contenido de disclaimer.html
+      fetch('disclaimer.html')
+        .then(response => {
+          if (!response.ok) {
+            throw new Error('Error al cargar el archivo: ' + response.statusText);
+          }
+          return response.text();
+        })
+        .then(data => {
+          document.getElementById('main').innerHTML = data; // Cargar el contenido en el div
+        })
+        .catch(err => console.error('Error:', err));
     });
 });
