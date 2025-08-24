9720e41a7ea6e0329b1e04328db4256f21ccccb5
JabraScan <Jabracadabrap@gmail.com>
2025-08-25 00:35:41 +0200

    Update libro-ficha.js

--- js/libro-ficha.js
+++ js/libro-ficha.js
@@ -21,107 +21,60 @@ function cargarlibro(libroId) {
                     document.body.innerHTML = '<p>Obra no encontrada.</p>';
                     return;
                 }
-          const clave = obra.querySelector("clave").textContent.trim();
-          const nombreobra = obra.querySelector("nombreobra").textContent.trim();
-          const autor = obra.querySelector("autor").textContent.trim();
-          const sinopsis = obra.querySelector("sinopsis").textContent.trim();
-          const imagen = obra.querySelector("imagen").textContent.trim();
-          const valoracion = obra.querySelector("valoracion").textContent.trim();
-          const tipoobra = obra.querySelector("tipoobra").textContent.trim();
-          const Categoria = obra.querySelector("categoria").textContent.trim();
-          const estado = obra.querySelector("estado").textContent.trim();
-          const ubicacion = obra.querySelector("ubicacion").textContent.trim();
+				const clave = obra.querySelector("clave").textContent.trim();
+				const nombreobra = obra.querySelector("nombreobra").textContent.trim();
+				const autor = obra.querySelector("autor").textContent.trim();
+				const sinopsis = obra.querySelector("sinopsis").textContent.trim();
+				const imagen = obra.querySelector("imagen").textContent.trim();
+				const valoracion = obra.querySelector("valoracion").textContent.trim();
+				const tipoobra = obra.querySelector("tipoobra").textContent.trim();
+				const Categoria = obra.querySelector("categoria").textContent.trim();
+				const estado = obra.querySelector("estado").textContent.trim();
+				const ubicacion = obra.querySelector("ubicacion").textContent.trim();
 
-          const DataBook = document.querySelector('.book-card');
-            const headerDataBook = document.createElement("div");
-                headerDataBook.className = "book-header";
-                headerDataBook.innerHTML = `
-                    <i class="fa-solid fa-book"></i> ${nombreobra.toUpperCase()}
-	              `;
+				const DataBook = document.querySelector('.book-card');
+					const headerDataBook = document.createElement("div");
+						headerDataBook.className = "book-header";
+						headerDataBook.innerHTML = `
+							<i class="fa-solid fa-book"></i> ${nombreobra.toUpperCase()}
+						`;
 
-            const mainDataBook = document.createElement("div");
-                mainDataBook.className = "book-main";
-                mainDataBook.innerHTML = `
-                      <div class="book-image">
-                        <img src="../../img/${imagen}" alt="${nombreobra}"/>
-                        <div class="book-genres">
-                            <span><i class="fa-solid fa-tags"></i>${Categoria}</span>                
-                        </div>
-                        <div class="book-links">
-                          <a href="#"><i class="fa-solid fa-book"></i> ${tipoobra}</a>
-                          <a href="#"><i class="fa-solid fa-globe"></i> ${ubicacion}</a>
-                          <a href="#"><i class="fa-solid fa-clock"></i> ${estado}</a>
-                        </div>
-                      </div>
-					  <div class="book-info-container">
-                      		<div class="book-info">
-                            	<h2 id="nombre-obra">${nombreobra}</h2>
-                            </div>
-                            <div class="book-synopsis">
-                              <b><i class="fa-solid fa-info-circle"></i> Sinopsis:</b>
-                              <p id="sinopsis-obra">${sinopsis}</p>
-                            </div>
-                            <div class="book-buttons">
-                              <button class="chapter-list"><i class="fa-solid fa-list"></i> Lista de capítulos</button>
-                              <button class="read-first"><i class="fa-solid fa-play"></i> Empezar a leer</button>
-                            </div>
-                      </div>
-	              `;
-            DataBook.prepend(mainDataBook);
-            DataBook.prepend(headerDataBook);
-
-                            /*
-							<div class="book-rating" id="book-rating">
-                              <span class="stars" data-rating="4.5">
-                                  <i class="fa-solid fa-star"></i>
-                                  <i class="fa-solid fa-star"></i>
-                                  <i class="fa-solid fa-star"></i>
-                                  <i class="fa-solid fa-star"></i>
-                                  <i class="fa-solid fa-star-half-stroke"></i>
-                              </span>
-                              <span class="rating-value">4.5/5</span>
-                              <span class="rating-votes">(345 votos)</span>
-                              <span class="rate-btns">
-                                <button class="rate-star" data-value="1"><i class="fa-solid fa-star"></i></button>
-                                <button class="rate-star" data-value="2"><i class="fa-solid fa-star"></i></button>
-                                <button class="rate-star" data-value="3"><i class="fa-solid fa-star"></i></button>
-                                <button class="rate-star" data-value="4"><i class="fa-solid fa-star"></i></button>
-                                <button class="rate-star" data-value="5"><i class="fa-solid fa-star"></i></button>
-                              </span>
-							  */
-        /*
-            document.querySelector(".book-header").innerHTML = `<i class="fa-solid fa-book"></i> ${nombreobra.toUpperCase()}`;
-            document.getElementById('imagen-obra').textContent = '../../img/${imagen}';
-            document.getElementById('nombre-obra').innerHTML =  `
-                        ${nombreobra}
-                        <span class="book-author">por <b id="nombre-autor">${autor}</b></span>
-                        `;
-            document.getElementById('nombre-autor').textContent = autor;
-            document.getElementById('sinopsis-obra').textContent = sinopsis;
-            document.querySelector(".book-genres").innerHTML =`<span><i class="fa-solid fa-tags"></i>${Categoria}</span>`;
-                
-            const DataBook = document.getElementById('book-links');
-                const itemDataBook = document.createElement("div");
-                    itemDataBook.innerHTML =  `
-                        <span><i class="fa-solid fa-globe"></i> ${tipoobra}</span>
-                        <span>
-                        <i class="fa-solid fa-book"></i> Divine Novel</a>${tipoobra}
-                        `;
-                DataBook.appendChild(itemDataBook);
-        */
-                /*
-                document.getElementById('imagen-obra').textContent = libro.getElementsByTagName('imagen')[0].textContent;
-                document.getElementById('nombre-obra').textContent = libro.getElementsByTagName('nombreobra')[0].textContent;
-                document.getElementById('nombre-autor').textContent = libro.getElementsByTagName('titulo')[0].textContent;
-                document.getElementById('sinopsis-obra').textContent = libro.getElementsByTagName('autor')[0].textContent;
-                // ...otros campos
-                */
-            });
+					const mainDataBook = document.createElement("div");
+						mainDataBook.className = "book-main";
+						mainDataBook.innerHTML = `
+							<div class="book-image">
+								<img src="../../img/${imagen}" alt="${nombreobra}"/>
+								<div class="book-genres">
+									<span><i class="fa-solid fa-tags"></i>${Categoria}</span>                
+								</div>
+								<div class="book-links">
+								<a href="#"><i class="fa-solid fa-book"></i> ${tipoobra}</a>
+								<a href="#"><i class="fa-solid fa-globe"></i> ${ubicacion}</a>
+								<a href="#"><i class="fa-solid fa-clock"></i> ${estado}</a>
+								</div>
+							</div>
+							<div class="book-info-container">
+									<div class="book-info">
+										<h2 id="nombre-obra">${nombreobra}</h2>
+									</div>
+									<div class="book-synopsis">
+									<b><i class="fa-solid fa-info-circle"></i> Sinopsis:</b>
+									<p id="sinopsis-obra">${sinopsis}</p>
+									</div>
+									<div class="book-buttons">
+									<button class="chapter-list"><i class="fa-solid fa-list"></i> Lista de capítulos</button>
+									<button class="read-first"><i class="fa-solid fa-play"></i> Empezar a leer</button>
+									</div>
+							</div>
+						`;
+					DataBook.prepend(mainDataBook);
+					DataBook.prepend(headerDataBook);
+            	});
 //    });
+}
     // Datos de ejemplo
     const chapters = Array.from({length: 80}, (_, i) => `Capítulo ${i+1}: Título del capítulo`);
     const latestChaptersCount = 6;
-}
     // -------- ESTRELLAS Y VALORACIÓN --------
     function renderStars(el, rating) {
       let stars = '';
@@ -142,8 +95,8 @@ function cargarlibro(libroId) {
     }
     
     // Renderizar estrellas actuales
-    const ratingContainer = document.querySelector('.stars');
-    renderStars(ratingContainer, getBookRating());
+    //const ratingContainer = document.querySelector('.stars');
+    //renderStars(ratingContainer, getBookRating());
     
     // Valoración solo una vez por usuario (localStorage)
     document.querySelectorAll('.rate-star').forEach(btn => {
