756ecab59b6b3c9551574998152f202fd3c9e9c2
JabraScan <Jabracadabrap@gmail.com>
2025-08-27 06:42:33 +0200

    Update ultimoscapitulos.js

--- js/ultimoscapitulos.js
+++ js/ultimoscapitulos.js
@@ -81,9 +81,9 @@ const render = () => {
            data-pdf-obra="${item._obra}"
            data-pdf-capitulo="${item.numCapitulo}"
            class="pdf-link">
-            <span class="fecha">${formatDateEs(item._fecha)}</span> - 
-            <span class="obra">${item._obra}</span> - 
-            <span class="cap">Capítulo ${item.numCapitulo}</span> - 
+            <span class="fecha">${formatDateEs(item._fecha)}</span>
+            <span class="obra">${item._obra}</span>
+            <span class="cap">Capítulo ${item.numCapitulo}</span>
             <span class="titulo">${item.nombreCapitulo}</span>
         </a>
       `;
