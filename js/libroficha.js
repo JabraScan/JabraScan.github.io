23d8243a597a8e8c9cdb12279544c56912de8a49
JabraScan <Jabracadabrap@gmail.com>
2025-08-31 19:10:05 +0200

    Update libroficha.js

--- js/libroficha.js
+++ js/libroficha.js
@@ -95,7 +95,7 @@ export function cargarlibro(libroId) {
             `;
       
       leerVisitas(`obra_${clave}`).then(vis => {
-          const visitas = vis === -1 ? 0 : vis;
+          const visitas = vis === -1 ? 1 : vis+1;
             const numVisitas = document.createElement("a");
                   numVisitas.innerHTML = `<a href="#"><i class="fa-solid fa-eye"  ></i> ${visitas} veces</a>`;
                   const booklinks  = mainDataBook.querySelector('.book-links');
@@ -234,6 +234,7 @@ function renderCapitulos(listacapitulos, clave, seccionUltimos, ordenActual = "a
     renderCapitulos(listacapitulos, clave, "", nuevoOrden);
   });
 }
+
 
 
 
