fa5db6d2d17445b683c47d1c4fb0cec7659a0a5e
JabraScan <Jabracadabrap@gmail.com>
2025-09-02 12:12:28 +0200

    Update leerobras.js

--- js/leerobras.js
+++ js/leerobras.js
@@ -147,7 +147,7 @@ document.addEventListener("DOMContentLoaded", function () {
                   const capitulosConObra = capitulos
                     .filter((cap, i) => {
                       const fechaCap = new Date(cap.Fecha);
-                      if (fechaCap > hoy) {
+                      if (parseDateDMY(fechaCap) > parseDateDMY(hoy)) {
                         console.info(`⏳ Capítulo "${cap.nombreCapitulo}" programado para el futuro (${cap.Fecha}), se omite.`);
                         return false;
                       }
