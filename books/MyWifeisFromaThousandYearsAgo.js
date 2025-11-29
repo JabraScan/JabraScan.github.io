6d12af53ea80a4744a3b659f6e13e451fefa5a25
JabraScan <Jabracadabrap@gmail.com>
2025-11-29 11:24:59 +0100

    Create MyWifeisFromaThousandYearsAgo.js

---
+++ books/MyWifeisFromaThousandYearsAgo.js
@@ -0,0 +1,6 @@
+{
+    "MyWifeisFromaThousandYearsAgo": [
+	  	{ "Fecha" : "29-11-2025", "NombreArchivo" : "MyWifeisFromaThousandYearsAgo-0001_JabraScan.pdf", "numCapitulo" :  "1", "nombreCapitulo" : "Soy una buena persona"                                                 	, "tituloObra" : "Mi Esposa Es De Hace Mil Años" },
+		  { "Fecha" : "30/11/2025", "NombreArchivo" : "MyWifeisFromaThousandYearsAgo-0002_JabraScan.pdf", "numCapitulo" :  "2", "nombreCapitulo" : "Esto es un malentendido"                                               	, "tituloObra" : "Mi Esposa Es De Hace Mil Años" }
+    ]
+}
