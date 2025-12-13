85073d897bfce754c448ed767b152748831ab904
JabraScan <Jabracadabrap@gmail.com>
2025-12-13 20:49:34 +0100

    Update biblioteca.js

--- js/biblioteca.js
+++ js/biblioteca.js
@@ -25,8 +25,6 @@ const TARGET_USER_TAVO = 'dcffc6ac-c4e5-4ab7-86da-5e55c982ad97';
           btnOcultar.textContent = '–';
           btnOcultar.className = 'btn btn-sm btn-outline-secondary';
           btnOcultar.addEventListener('click', () => {
-            bavatars.classList.add('d-none');
-            bdemo.classList.remove('d-none');
             col.className = 'col-sm-6 col-md-4 col-lg-3';
             btnOcultar.style.display = 'none';
             btnMostrar.style.display = 'inline-block';
@@ -38,6 +36,8 @@ const TARGET_USER_TAVO = 'dcffc6ac-c4e5-4ab7-86da-5e55c982ad97';
           btnMostrar.className = 'btn btn-sm btn-outline-secondary';
           btnMostrar.style.display = 'none'; // empieza oculto
           btnMostrar.addEventListener('click', () => {
+            bavatars.classList.add('d-none');
+            bdemo.classList.remove('d-none');
             bavatars.classList.remove('d-none');
             bdemo.classList.add('d-none');
             col.className = 'col-12';
@@ -384,7 +384,7 @@ async function cargarTiendaAvatar() {
                 // 🔹 Crear los nuevos botones en el mismo sitio donde estaba el +
                 if (contenedorBoton && body) {
                   // Aquí está la llamada integrada
-                  crearBotonesToggle(contenedorBoton, col, bminiature, bavatars);
+                  crearBotonesToggle(contenedorBoton, col, bavatars, bminiature);
                 }
               } catch (err) {
                 btnMas.disabled = false;
