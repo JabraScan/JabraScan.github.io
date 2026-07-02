5696b5025f490b40121d41b53619f3cb94728fee
JabraScan <Jabracadabrap@gmail.com>
2026-07-03 01:47:52 +0200

    Update lectorpdfmod.js

--- js/lectorpdfmod.js
+++ js/lectorpdfmod.js
@@ -133,7 +133,18 @@ function cargarPDF(clave, nombreArchivo, paginaInicial, idx, capitulosObra) {
   //añadido el 09-09-2025 23:28 para gestionar los capitulos mas alla de 999
       // Extraemos el número de capítulo actual
       const numCapitulo = capitulosObra[idx].numCapitulo;
-      const server = capitulosObra[idx]._server;
+      //const server = (capitulosObra[idx]._server || "io-pdfs"); 
+      /*const server = (
+                        ["CDMNQTMHC", "CultivationOnline", "DualCultivation", "PathtoDualCultivation"]
+                          .includes(clave)
+                      ) ? "local" : "io-pdfs";
+      */
+      const server = ["local", "io-pdfs"].includes(capitulosObra[idx]._server)
+        ? capitulosObra[idx]._server
+        : (["CDMNQTMHC","CultivationOnline","DualCultivation","PathtoDualCultivation"].includes(clave)
+            ? "local"
+            : "io-pdfs");
+
       // Si el número es mayor a 999, tomamos todos los dígitos excepto los tres últimos.
       // Ejemplo: 1203 → "1", 123653 → "123"
       const extra = numCapitulo > 999 ? String(numCapitulo).slice(0, -3) : "";
